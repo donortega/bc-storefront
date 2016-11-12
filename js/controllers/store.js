0f2e1f6c47d3575ff9c6ef7450f7cb7b2728aa7f
Don Ortega <don@donortega.com>
2016-11-11 23:29:11 -0800

    Product: Add to Cart functionality

--- js/controllers/store.js
+++ js/controllers/store.js
@@ -6,6 +6,12 @@ app.controller('StoreCtrl', ['$scope', 'products', function($scope, products) {
     self.cartCount = 0;
     self.qtyToAdd = 1;
 
+    if (!sessionStorage.getItem('bigcommerce-cart')) {
+        self.cart = [];
+    } else {
+        self.cart = JSON.parse(sessionStorage.getItem('bigcommerce-cart'));
+    }
+
     products.getList().then(function(data) {
         self.productList = data;
     });
@@ -14,6 +20,24 @@ app.controller('StoreCtrl', ['$scope', 'products', function($scope, products) {
         if (self.qtyToAdd > 1) {
             self.qtyToAdd--;
         }
+    };
+
+    self.addCart = function(product, qtyToAdd) {
+        // { product, qtyToAdd }
+        var index = self.cart.findIndex(function(element) {
+            return angular.equals(element.product, product);
+        });
+
+        if (index === -1) {
+            self.cart.push({
+                product: product,
+                qty: qtyToAdd
+            });
+        } else {
+            self.cart[index].qty = self.cart[index].qty + qtyToAdd;
+        }
+
+        sessionStorage.setItem('bigcommerce-cart', JSON.stringify(self.cart));
     };
 
 }]);
