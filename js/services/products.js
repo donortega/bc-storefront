a6c861631842cd0e86fdd30cb06a9eacc7c8fb9d
Don Ortega <don@donortega.com>
2016-11-12 09:49:54 -0800

    use $q constructor approach for promises

--- js/services/products.js
+++ js/services/products.js
@@ -4,25 +4,24 @@ app.service('products', ['$http', '$q', function($http, $q) {
     var service = {};
 
     service.getList = function() {
-        var deferred = $q.defer();
-
-        if (!sessionStorage.getItem('bigcommerce-productList')) {
-            $http.get('/assets/json/products.json').then(
-                function(response) {
-                    // success
-                    sessionStorage.setItem('bigcommerce-productList', JSON.stringify(response.data));
-                    deferred.resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
-                },
-                function(response) {
-                    // failure
-                    console.error('Failed to retrieve JSON:', response);
-                }
-            );
-        } else {
-            deferred.resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
-        }
-
-        return deferred.promise;
+        return $q(function(resolve, reject) {
+            if (!sessionStorage.getItem('bigcommerce-productList')) {
+                $http.get('/assets/json/products.json').then(
+                    function(response) {
+                        // success
+                        sessionStorage.setItem('bigcommerce-productList', JSON.stringify(response.data));
+                        resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
+                    },
+                    function(response) {
+                        // failure
+                        console.error('Failed to retrieve JSON:', response);
+                        reject(response);
+                    }
+                );
+            } else {
+                resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
+            }
+        });
     };
 
     return service;
