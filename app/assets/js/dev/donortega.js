4d943150087f2d1ec332061a8590ee2803848601
Don Ortega <don@donortega.com>
2016-11-14 16:09:58 -0800

    revert Products service to return Q-stlye promise

--- app/assets/js/dev/donortega.js
+++ app/assets/js/dev/donortega.js
@@ -168,27 +168,29 @@ app.service('products', ['$http', '$q', function($http, $q) {
     var service = {};
 
     service.getList = function() {
-        return $q(function(resolve, reject) {
-            // check if products JSON has already been cached in sessionStorage
-            if (!sessionStorage.getItem('bigcommerce-productList')) {
-                // products JSON not in sessionStorage, go fetch data then place in sessionStorage
-                $http.get('/assets/json/products.json').then(
-                    function(response) {
-                        // success
-                        sessionStorage.setItem('bigcommerce-productList', JSON.stringify(response.data));
-                        resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
-                    },
-                    function(response) {
-                        // failure
-                        console.error('Failed to retrieve JSON:', response);
-                        reject(response);
-                    }
-                );
-            } else {
-                // retrieve cached products JSON from sessionStorage
-                resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
-            }
-        });
+        var deferred = $q.defer();
+
+        // check if products JSON has already been cached in sessionStorage
+        if (!sessionStorage.getItem('bigcommerce-productList')) {
+            // products JSON not in sessionStorage, go fetch data then place in sessionStorage
+            $http.get('/assets/json/products.json').then(
+                function(response) {
+                    // success
+                    sessionStorage.setItem('bigcommerce-productList', JSON.stringify(response.data));
+                    deferred.resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
+                },
+                function(response) {
+                    // failure
+                    console.error('Failed to retrieve JSON:', response);
+                    deferred.reject(response);
+                }
+            );
+        } else {
+            // retrieve cached products JSON from sessionStorage
+            deferred.resolve(JSON.parse(sessionStorage.getItem('bigcommerce-productList')));
+        }
+
+        return deferred.promise;
     };
 
     return service;
